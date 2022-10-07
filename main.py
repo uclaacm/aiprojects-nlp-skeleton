49966d452c1c7200971f9bac4fbc3ce890b08d41
Prateik Sinha <emailprateik@gmail.com>
2022-10-06 20:19:28 -0700

    Moved picking optimizer and loss to main

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import constants
 from data.StartingDataset import StartingDataset
 from networks.StartingNetwork import StartingNetwork
 from train_functions.starting_train import starting_train
+import torch.nn as nn
+import torch.optim as optim
 
 
 def main():
@@ -20,12 +22,19 @@ def main():
     train_dataset = StartingDataset()
     val_dataset = StartingDataset()
     model = StartingNetwork()
+
+    # Initalize optimizer (for gradient descent) and loss function
+    optimizer = optim.Adam(model.parameters())
+    loss_fn = nn.CrossEntropyLoss()
+
     starting_train(
         train_dataset=train_dataset,
         val_dataset=val_dataset,
         model=model,
         hyperparameters=hyperparameters,
         n_eval=constants.N_EVAL,
+        optimizer=optimizer,
+        loss_fn=loss_fn
     )
 
 
