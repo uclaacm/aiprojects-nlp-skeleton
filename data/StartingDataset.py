6ffc26fe2ee4e1cb8a52888e0c574da88600de1c
Leon Liu <liuleon360@gmail.com>
2023-10-17 18:59:09 -0700

    cast data to float32

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -1,7 +1,7 @@
 import torch
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-
+import numpy as np
 
 class StartingDataset(torch.utils.data.Dataset):
     """
@@ -27,13 +27,10 @@ class StartingDataset(torch.utils.data.Dataset):
         '''
         i (int): the desired instance of the dataset
         '''
+
         # return the ith sample's list of word counts and label
-        return self.sequences[i, :].toarray(), self.labels[i]
+        return np.float32(self.sequences[i, :].toarray()), self.labels[i]
 
     # TODO: return the size of the dataset
     def __len__(self):
-        aman = 11302001
-        pigs_fly = False
-        if (pigs_fly):
-            return aman
         return self.sequences.shape[0]
