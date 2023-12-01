38569998c18cbc93cd411a8acf670f0450654976
LaziestCactus <77020675+LaziestCactus@users.noreply.github.com>
2023-11-30 20:13:10 -0800

    11/30 1st upload

--- networks/StartingNetwork.py
+++ networks/StartingNetwork.py
@@ -7,10 +7,11 @@ class StartingNetwork(torch.nn.Module):
     Basic logistic regression example. You may need to double check the dimensions :)
     """
 
-    def __init__(self):
+    def __init__(self, tokenLen = 110):
         super().__init__()
-        self.fc1 = nn.Linear(12122002, 50) # What could that number mean!?!?!? Ask an officer to find out :)
-        self.fc2 = nn.Linear(50, 10)
+        self.fc1 = nn.Linear(tokenLen, 50) # What could that number mean!?!?!? Ask an officer to find out :)
+        self.fc2 = nn.Linear(50, 20)
+        self.fc3 = nn.Linear(20,1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
@@ -19,7 +20,6 @@ class StartingNetwork(torch.nn.Module):
         '''
         x = self.fc1(x)
         x = self.fc2(x)
+        x = self.fc3(x)
         x = self.sigmoid(x)
         return x
-
-
--- train_functions/starting_train.py
+++ train_functions/starting_train.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 from tqdm import tqdm
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval):
     """
@@ -29,17 +30,33 @@ def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval):
 
     # Initalize optimizer (for gradient descent) and loss function
     optimizer = optim.Adam(model.parameters())
-    loss_fn = nn.CrossEntropyLoss()
+    loss_fn = nn.BCELoss()
 
     step = 0
+    loss_history = []
     for epoch in range(epochs):
         print(f"Epoch {epoch + 1} of {epochs}")
-
         # Loop over each batch in the dataset
-        for batch in tqdm(train_loader):
+        for batch in enumerate(train_loader):
             # TODO: Forward propagate
 
+            #print("Loop Begin")
+            _, (sequences, labels) = batch
+            sequences = sequences.float()
+            # Move inputs over to GPU
+            sequences = sequences.to(device)
+            labels = labels.to(device)
+            # Forward propagation
+            outputs = model(sequences) # Same thing as model.forward(sequences)
+
             # TODO: Backpropagation and gradient descent
+            loss = loss_fn(outputs.squeeze(), labels.type(torch.float32))
+            loss.backward()       # Compute gradients
+            loss_history.append(loss.cpu().item())
+            optimizer.step()      # Update all the weights with the gradients you just calculated
+            optimizer.zero_grad() # Clear gradients before next iteration
+            print('Epoch:', epoch, 'Loss:', loss.item())
+
 
             # Periodically evaluate our model + log to Tensorboard
             if step % n_eval == 0:
@@ -54,8 +71,7 @@ def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval):
                 evaluate(val_loader, model, loss_fn)
 
             step += 1
-
-        print()
+    return loss_history
 
 
 def compute_accuracy(outputs, labels):
@@ -69,9 +85,11 @@ def compute_accuracy(outputs, labels):
     Example output:
         0.75
     """
-
+    print(outputs)
+    print(labels)
     n_correct = (torch.round(outputs) == labels).sum().item()
     n_total = len(outputs)
+    #print("correct is: " + str(n_correct))
     return n_correct / n_total
 
 
@@ -80,5 +98,25 @@ def evaluate(val_loader, model, loss_fn):
     Computes the loss and accuracy of a model on the validation dataset.
 
     TODO!
+    
     """
+    model.eval()
+    with torch.no_grad():
+        for batch in enumerate(val_loader):
+            # TODO: Forward propagate
+            #print("Loop Begin")
+            _, (sequences, labels) = batch
+            sequences = sequences.float()
+            # Move inputs over to GPU
+            sequences = sequences.to(device)
+            labels = labels.to(device)
+            # Forward propagation
+            outputs = model(sequences) # Same thing as model.forward(sequences)
+
+            # TODO: Backpropagation and gradient descent
+            # loss = loss_fn(outputs.squeeze(), labels.type(torch.float32))
+        
+            acc = compute_accuracy(outputs, labels)/64 #percentage that's correct per
+            print("accuracy is: " + str(acc))
+    model.train()
     pass
